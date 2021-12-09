dbb4413458d0fb92ac62b784002fbae7fd9f8959
Murad Buyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2021-12-09 13:40:10 +0100

    create script and readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # winget-script
 A list of programs I might install.
+
+## Installing Winget on Windows 10 LTSC (NO AUTO UPDATES):
+From the latest release download the .msixbundle and .xml file:
+https://github.com/microsoft/winget-cli/releases
+
+Open PowerShell and run (replace file paths to correct locations):
+
+Add-AppxProvisionedPackage -Online -PackagePath PATH TO MSIXBUNDLE -LicensePath PATH TO XML -Verbose
+
+Wait for install to finish and you're done.
---
+++ winget.ps1
@@ -0,0 +1,32 @@
+<# Not available in Winget:
+MSI Afterburner
+Throttlestop
+Tutanota
+#>
+
+winget install -e --id 7zip.7zip;
+winget install -e --id CPUID.CPU-Z;
+winget install -e --id Discord.Discord;
+winget install -e --id Docker.DockerDesktop;
+winget install -e --id Figma.Figma;
+winget install -e --id Git.Git;
+winget install -e --id Google.Chrome;
+winget install -e --id Intel.IntelDriverAndSupportAssistant;
+winget install -e --id KhronosGroup.VulkanSDK;
+winget install -e --id Microsoft.VisualStudioCode;
+winget install -e --id Microsoft.WindowsTerminal;
+winget install -e --id Mozilla.FirefoxDeveloperEdition;
+winget install -e --id Notion.Notion;
+winget install -e --id Nvidia.GeForceExperience;
+winget install -e --id OBSProject.OBSStudio;
+winget install -e --id OpenJS.Nodejs;
+winget install -e --id PostgreSQL.PostgreSQL;
+winget install -e --id ProtonTechnologies.ProtonVPN;
+winget install -e --id REALiX.HWiNFO;
+winget install -e --id Spotify.Spotify;
+winget install -e --id TechPowerUp.GPU-Z;
+winget install -e --id Valve.Steam;
+winget install -e --id VideoLAN.VLC;
+winget install -e --id dbeaver.dbeaver;
+winget install -e --id qBittorrent.qBittorrent;
+winget install -e --id vim.vim;
